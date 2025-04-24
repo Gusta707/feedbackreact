e333ea2096da2fab81306ad8e476401fa05f2ec8
gu7161612 <gu7161612@gmail.com>
2025-04-24 17:51:45 +0000

    mudança da função handleTrocarNota do CardInicial.jsx para o InputButton.jsx

--- src/Components/CardInicial.jsx
+++ src/Components/CardInicial.jsx
@@ -1,10 +1,6 @@
 import iconStar from "../assets/icon-star.svg"
 
 export function CardInicial({ nota, setNota, setSubmited }) {
-    function handleTrocarNota(nota) {
-        setNota(nota)
-      }
-
     function handleSubmit(){
         if (nota !== 0) {
           setSubmited(true)
@@ -17,7 +13,7 @@ export function CardInicial({ nota, setNota, setSubmited }) {
     return (
         <div className="max-w-103 bg-gradient-dark p-6 rounded-2xl text-white font-overpass">
             <div className="bg-dark-blue p-4 w-fit rounded-full mb-4">
-            <img src={iconStar} alt="icon star" />
+                <img src={iconStar} alt="icon star" />
             </div>
 
             <h1 className="text-2xl font-bold mb-2.5">How did we do?</h1>
@@ -25,12 +21,12 @@ export function CardInicial({ nota, setNota, setSubmited }) {
             <p className="text-sm text-light-grey mb-6">Please let us know how we did with your support request. All feedback is appreciated to help us improve our offering!</p>
 
             <div className="flex justify-between mb-6">
-                <InputButton valor={1} handleTrocarNota={handleTrocarNota}/>
-                <InputButton valor={2} handleTrocarNota={handleTrocarNota}/>
-                <InputButton valor={3} handleTrocarNota={handleTrocarNota}/>
-                <InputButton valor={4} handleTrocarNota={handleTrocarNota}/>
-                <InputButton valor={5} handleTrocarNota={handleTrocarNota}/>
-            </div>
+                <InputButton valor={1} setNota={setNota} />
+                <InputButton valor={2} setNota={setNota} />
+                <InputButton valor={3} setNota={setNota} />
+                <InputButton valor={4} setNota={setNota} />
+                <InputButton valor={5} setNota={setNota} />
+            </div> 
 
             <button onClick={handleSubmit} className="bg-orange w-full py-3 rounded-3xl uppercase text-sm tracking-1 font-bold hover:bg-white hover:text-orange cursor-pointer">Submit</button>
       </div>
